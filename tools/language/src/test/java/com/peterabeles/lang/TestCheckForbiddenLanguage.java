01ef64cb48df54a3fbe2b20c74c0de3aebca16a9
Peter Abeles <peter.abeles@gmail.com>
2024-05-10 20:51:31 -0500

    Language   ignore_below defaults to one line   ignore_below reports an error if number isn't a number

--- tools/language/src/test/java/com/peterabeles/lang/TestCheckForbiddenLanguage.java
+++ tools/language/src/test/java/com/peterabeles/lang/TestCheckForbiddenLanguage.java
@@ -13,99 +13,132 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class TestCheckForbiddenLanguage {
 
-	@Test void disableAll() {
-		String D = CheckForbiddenLanguage.DEFAULT_IDENTIFIER;
-		String L = CheckForbiddenLanguage.DISABLE_ALL;
-		String comment = "//" + D + " " + L;
+    @Test
+    void disableAll() {
+        String D = CheckForbiddenLanguage.DEFAULT_IDENTIFIER;
+        String L = CheckForbiddenLanguage.DISABLE_ALL;
+        String comment = "//" + D + " " + L;
 
-		var alg = new CheckForbiddenLanguage();
-		CheckForbiddenHelper.addForbiddenFunction(alg, "forbidden", "Because");
+        var alg = new CheckForbiddenLanguage();
+        CheckForbiddenHelper.addForbiddenFunction(alg, "forbidden", "Because");
 
-		assertTrue(alg.process(comment + "\n\n 1\nfoo.forbidden()"));
-	}
+        assertTrue(alg.process(comment + "\n\n 1\nfoo.forbidden()"));
+    }
 
-	@Test void disableCheck() {
-		String D = CheckForbiddenLanguage.DEFAULT_IDENTIFIER;
-		String L = CheckForbiddenLanguage.DISABLE_CHECK;
-		String comment = "//" + D + " " + L + " function_forbidden";
+    @Test
+    void disableCheck() {
+        String D = CheckForbiddenLanguage.DEFAULT_IDENTIFIER;
+        String L = CheckForbiddenLanguage.DISABLE_CHECK;
+        String comment = "//" + D + " " + L + " function_forbidden";
 
-		var alg = new CheckForbiddenLanguage();
-		CheckForbiddenHelper.addForbiddenFunction(alg, "forbidden", "Because");
+        var alg = new CheckForbiddenLanguage();
+        CheckForbiddenHelper.addForbiddenFunction(alg, "forbidden", "Because");
 
-		assertTrue(alg.process(comment + "\n\n 1\nfoo.forbidden()"));
-		assertFalse(alg.process("foo.forbidden()\n" + comment + "\n\n 1\nfoo.forbidden()"));
-	}
+        assertTrue(alg.process(comment + "\n\n 1\nfoo.forbidden()"));
+        assertFalse(alg.process("foo.forbidden()\n" + comment + "\n\n 1\nfoo.forbidden()"));
+    }
 
-	@Test void disableCheck_DoesNotExist() {
-		String D = CheckForbiddenLanguage.DEFAULT_IDENTIFIER;
-		String L = CheckForbiddenLanguage.DISABLE_CHECK;
-		String comment = "//" + D + " " + L + " function_yolo";
+    @Test
+    void disableCheck_DoesNotExist() {
+        String D = CheckForbiddenLanguage.DEFAULT_IDENTIFIER;
+        String L = CheckForbiddenLanguage.DISABLE_CHECK;
+        String comment = "//" + D + " " + L + " function_yolo";
 
-		var alg = new CheckForbiddenLanguage();
-		CheckForbiddenHelper.addForbiddenFunction(alg, "forbidden", "Because");
+        var alg = new CheckForbiddenLanguage();
+        CheckForbiddenHelper.addForbiddenFunction(alg, "forbidden", "Because");
 
-		assertFalse(alg.process(comment + "\n\n 1\nfoo.asdf()"));
-		assertSame(CheckForbiddenLanguage.MALFORMED_COMMAND, alg.getFailures().get(0).check);
-	}
+        assertFalse(alg.process(comment + "\n\n 1\nfoo.asdf()"));
+        assertSame(CheckForbiddenLanguage.MALFORMED_COMMAND, alg.getFailures().get(0).check);
+    }
 
-	@Test void ignoreBelow() {
-		// create a simple shorthand because of how verbose it would be otherwise
-		String D = CheckForbiddenLanguage.DEFAULT_IDENTIFIER;
-		String L = CheckForbiddenLanguage.IGNORE_BELOW;
-		String comment = "//" + D + " " + L;
+    @Test
+    void ignoreBelow() {
+        // create a simple shorthand because of how verbose it would be otherwise
+        String D = CheckForbiddenLanguage.DEFAULT_IDENTIFIER;
+        String L = CheckForbiddenLanguage.IGNORE_BELOW;
+        String comment = "//" + D + " " + L;
 
-		var alg = new CheckForbiddenLanguage();
-		CheckForbiddenHelper.addForbiddenFunction(alg, "forbidden", "Because");
-		assertTrue(alg.process(comment + " 1\nfoo.forbidden()"));
-		assertTrue(alg.process(comment + " 1\nfoo.forbidden()\n"));
-		assertFalse(alg.process(comment + " 1\n\nfoo.forbidden()"));
-		assertEquals(2, alg.getFailures().get(0).line);
-		assertEquals(3, alg.getFailures().get(1).line);
-		assertFalse(alg.process(comment + " 0\nfoo.forbidden()"));
-		assertEquals(1, alg.getFailures().get(0).line);
-		assertEquals(2, alg.getFailures().get(1).line);
-		assertFalse(alg.process(comment + " 0\nfoo.forbidden()\n"));
-		assertEquals(1, alg.getFailures().get(0).line);
-		assertEquals(2, alg.getFailures().get(1).line);
-	}
+        var alg = new CheckForbiddenLanguage();
+        CheckForbiddenHelper.addForbiddenFunction(alg, "forbidden", "Because");
+        assertTrue(alg.process(comment + " 1\nfoo.forbidden()"));
+        assertTrue(alg.process(comment + " 1\nfoo.forbidden()\n"));
+        assertFalse(alg.process(comment + " 1\n\nfoo.forbidden()"));
+        assertEquals(2, alg.getFailures().get(0).line);
+        assertEquals(3, alg.getFailures().get(1).line);
+        assertFalse(alg.process(comment + " 0\nfoo.forbidden()"));
+        assertEquals(1, alg.getFailures().get(0).line);
+        assertEquals(2, alg.getFailures().get(1).line);
+        assertFalse(alg.process(comment + " 0\nfoo.forbidden()\n"));
+        assertEquals(1, alg.getFailures().get(0).line);
+        assertEquals(2, alg.getFailures().get(1).line);
 
-	/**
-	 * If there's a disable comment AND there is nothing to disable, that should be an error too
-	 */
-	@Test void ignoreBelow_error_if_no_error() {
-		// create a simple shorthand because of how verbose it would be otherwise
-		String D = CheckForbiddenLanguage.DEFAULT_IDENTIFIER;
-		String L = CheckForbiddenLanguage.IGNORE_BELOW;
-		String comment = "//" + D + " " + L;
+        // Default is one line
+        assertFalse(alg.process(comment + "\nfoo.forbidden()\nfoo.forbidden()"));
+        assertEquals(1, alg.getFailures().size());
+        assertEquals(3, alg.getFailures().get(0).line);
+    }
 
-		var alg = new CheckForbiddenLanguage();
-		CheckForbiddenHelper.addForbiddenFunction(alg, "forbidden", "Because");
-		assertFalse(alg.process(comment + " 1\n\n\n"));
-	}
+    /**
+     * If the ignore statement is badly formatted it should be an error
+     */
+    @Test
+    void ignoreBelow_bad_number() {
+        // create a simple shorthand because of how verbose it would be otherwise
+        String D = CheckForbiddenLanguage.DEFAULT_IDENTIFIER;
+        String L = CheckForbiddenLanguage.IGNORE_BELOW;
+        String comment = "//" + D + " " + L;
 
-	@Test void ignoreLine() {
-		// create a simple shorthand because of how verbose it would be otherwise
-		String D = CheckForbiddenLanguage.DEFAULT_IDENTIFIER;
-		String L = CheckForbiddenLanguage.IGNORE_LINE;
-		String comment = "//" + D + " " + L;
+        var alg = new CheckForbiddenLanguage();
+        CheckForbiddenHelper.addForbiddenFunction(alg, "forbidden", "Because");
+        assertFalse(alg.process(comment + " A\nfoo.forbidden()"));
+        assertEquals(2, alg.getFailures().size());
+        assertEquals(1, alg.getFailures().get(0).line);
+        assertEquals(2, alg.getFailures().get(1).line);
+    }
 
-		var alg = new CheckForbiddenLanguage();
-		CheckForbiddenHelper.addForbiddenFunction(alg, "forbidden", "Because");
-		assertTrue(alg.process("foo.forbidden() " + comment));
-		assertTrue(alg.process("foo.forbidden()" + comment));
-		assertTrue(alg.process("\nfoo.forbidden()" + comment));
-		assertTrue(alg.process("foo.forbidden()" + comment + "\n"));
-		assertTrue(alg.process("foo.forbidden()" + comment + "\n\n"));
 
-		// nothing is ignored
-		assertFalse(alg.process("foo.asdf()" + comment));
-	}
+    /**
+     * If there's a disable comment AND there is nothing to disable, that should be an error too
+     */
+    @Test
+    void ignoreBelow_error_if_no_error() {
+        // create a simple shorthand because of how verbose it would be otherwise
+        String D = CheckForbiddenLanguage.DEFAULT_IDENTIFIER;
+        String L = CheckForbiddenLanguage.IGNORE_BELOW;
+        String comment = "//" + D + " " + L;
 
-	/** If there's a multi line comment in the middle of a line it is removed and converted into a space */
-	@Test void multiLineCommentConvertedToSpace() {
-		var alg = new CheckForbiddenLanguage();
-		CheckForbiddenHelper.addForbiddenFunction(alg, "forbidden", "Because");
+        var alg = new CheckForbiddenLanguage();
+        CheckForbiddenHelper.addForbiddenFunction(alg, "forbidden", "Because");
+        assertFalse(alg.process(comment + " 1\n\n\n"));
+    }
 
-		assertFalse(alg.process("foo./*asdf*/forbidden()"));
-	}
+    @Test
+    void ignoreLine() {
+        // create a simple shorthand because of how verbose it would be otherwise
+        String D = CheckForbiddenLanguage.DEFAULT_IDENTIFIER;
+        String L = CheckForbiddenLanguage.IGNORE_LINE;
+        String comment = "//" + D + " " + L;
+
+        var alg = new CheckForbiddenLanguage();
+        CheckForbiddenHelper.addForbiddenFunction(alg, "forbidden", "Because");
+        assertTrue(alg.process("foo.forbidden() " + comment));
+        assertTrue(alg.process("foo.forbidden()" + comment));
+        assertTrue(alg.process("\nfoo.forbidden()" + comment));
+        assertTrue(alg.process("foo.forbidden()" + comment + "\n"));
+        assertTrue(alg.process("foo.forbidden()" + comment + "\n\n"));
+
+        // nothing is ignored
+        assertFalse(alg.process("foo.asdf()" + comment));
+    }
+
+    /**
+     * If there's a multi line comment in the middle of a line it is removed and converted into a space
+     */
+    @Test
+    void multiLineCommentConvertedToSpace() {
+        var alg = new CheckForbiddenLanguage();
+        CheckForbiddenHelper.addForbiddenFunction(alg, "forbidden", "Because");
+
+        assertFalse(alg.process("foo./*asdf*/forbidden()"));
+    }
 }
